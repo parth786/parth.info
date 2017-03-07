01db7aacd3c9619f741b21c357b2df915b7f677b
parth786 <parthrocks89@yahoo.in>
2017-03-07 10:12:00 -0500

    final 1

---
+++ javascript.js
@@ -0,0 +1,21 @@
+$(function(){
+    
+    
+    $("#cover_jumbotron").hide();
+    $("#cover_jumbotron").fadeIn(1500);
+//    $("#cover_jumbotron").queue(function(){
+//        $("#my_name").addClass("animated flash");
+//        $("#my_thaught").addClass("animated shake");
+//    
+//    });
+    
+//    
+//    $(".project_card").hover(function(){
+//        $(this).addClass("animated jello");
+//    });
+//    
+//    $(".project_card").mouseleave(function(){
+//        $(this).removeClass("animated jello");
+//    });
+    
+});
